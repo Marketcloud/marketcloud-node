0e3ad5f6e689914ea824ab5c10d89249f1fb526c
Mattia Alfieri <cikkense@gmail.com>
2017-04-23 21:32:20 +0200

    added variables endpoint

---
+++ src/endpoints/variables.js
@@ -0,0 +1,22 @@
+module.exports = (function() {
+
+
+
+  var Resource = require('./resource.js');
+  function Variables(master) {
+
+    Resource.call(this,master);
+
+    this.name = 'variables';
+    this.endpoint = '/'+this.name;
+  }
+
+
+
+  Variables.prototype = new Resource();
+
+
+
+  return Variables;
+
+})();
